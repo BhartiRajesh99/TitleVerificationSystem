dc6809a9ee0de665534c8debc29244ffc04d5398
RAJ <rajeshbharti1110@gmail.com>
2025-07-08 19:09:39 +0530

    fix cors error

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -115,7 +115,7 @@ const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
       path: "/",
     });
