ebc2d445f2cf9f2f774bd6e6dab84248792e41b3
Rajesh <rajeshbharti1110@gmail.com>
2026-01-13 00:36:22 +0530

    update routes in app or main file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,63 +1,16 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router";
-import IntroductionPage from "./pages/IntroductionPage";
-import AuthPage from "./pages/AuthPage";
-import VerificationPage from "./pages/VerificationPage";
-import PrivateRoute from "./components/PrivateRoute";
-import PublicRoute from "./components/PublicRoute";
 import { AuthProvider } from "./context/AuthContext";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route
-        path=""
-        element={
-          <PublicRoute>
-            <IntroductionPage />
-          </PublicRoute>
-        }
-      />
-      <Route
-        path="auth"
-        element={
-          <PublicRoute>
-            <AuthPage />
-          </PublicRoute>
-        }
-      />
-      <Route
-        path="verify"
-        element={
-          <PrivateRoute>
-            <VerificationPage />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="*"
-        element={
-          <PublicRoute>
-            <IntroductionPage />
-          </PublicRoute>
-        }
-      />
-    </Route>
-  )
-);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+      </BrowserRouter>
+  </React.StrictMode>
 );
