a245d48391efd270fc7f35b1bc6910c4d869dadd
Rajesh <rajeshbharti1110@gmail.com>
2026-01-19 18:38:48 +0530

    add smooth scroll to top

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,12 @@ import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 import { RequestsProvider } from "./context/RequestsContext";
+import ScrollToTop from "./components/ScrollToTop";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <AuthProvider>
         <RequestsProvider>
           <App />
